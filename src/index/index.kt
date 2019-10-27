3124edae3edfd50aaaa436d66f0cfef5978f6df0
Kotori Otonashi <57068968+IDOL99@users.noreply.github.com>
2019-10-28 01:19:42 +0900

    値の初期化をしてみました

--- src/index/index.kt
+++ src/index/index.kt
@@ -1,8 +1,33 @@
 package index
 
+import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.get
 import kotlin.browser.document
-import kotlin.dom.appendText
+
+val producer: HTMLInputElement?
+    get() = document.getElementsByName("producer")[0] as? HTMLInputElement
+val ritsuko: HTMLInputElement?
+    get() = document.getElementsByName("ritsuko")[0] as? HTMLInputElement
+val junjirou: HTMLInputElement?
+    get() = document.getElementsByName("junjirou")[0] as? HTMLInputElement
+val day: HTMLInputElement?
+    get() = document.getElementsByName("day")[0] as? HTMLInputElement
+val minHour: HTMLInputElement?
+    get() = document.getElementsByName("minHour")[0] as? HTMLInputElement
+val minMinute: HTMLInputElement?
+    get() = document.getElementsByName("minMinute")[0] as? HTMLInputElement
+val maxHour: HTMLInputElement?
+    get() = document.getElementsByName("maxHour")[0] as? HTMLInputElement
+val maxMinute: HTMLInputElement?
+    get() = document.getElementsByName("maxMinute")[0] as? HTMLInputElement
 
 fun main() {
-    document.getElementById("root")?.appendText("Hello, World!")
+    producer?.checked = false
+    ritsuko?.checked = false
+    junjirou?.checked = false
+    day?.value = "1"
+    minHour?.value = "12"
+    minMinute?.value = "0"
+    maxHour?.value = "13"
+    maxMinute?.value = "0"
 }
